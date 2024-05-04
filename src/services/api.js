a042495b4250b36c65e9d4becd691cf9db042a69
sajidbhatti786 <sbhattti1212@gmail.com>
2024-05-04 21:35:38 +0500

    server usrl changed to live

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:5500";
-// export const BASE_URL = "https://h2o-events-backend.vercel.app"
+// export const BASE_URL = "http://localhost:5500";
+export const BASE_URL = "https://h2o-events-backend.vercel.app"
