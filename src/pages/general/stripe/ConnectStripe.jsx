d5b9265593298ef9af5d17b00aba1e85b6d3662d
sajidbhatti786 <sbhattti1212@gmail.com>
2024-05-07 22:49:12 +0500

    customized stripe connect form

--- src/pages/general/stripe/ConnectStripe.jsx
+++ src/pages/general/stripe/ConnectStripe.jsx
@@ -6,10 +6,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
+
 function ConnectStripe() {
   const { token, userData } = useAuth();
-  const [connected, setConnected] = useState()
+  const [connected, setConnected] = useState();
   const navigate = useNavigate();
+
   useEffect(() => {
     const checkAccount = async () => {
       try {
@@ -29,23 +31,19 @@ function ConnectStripe() {
       }
     };
     checkAccount();
-  },[])
+  }, []);
+
   const [formData, setFormData] = useState({
-    country: '',
     email: '',
     businessType: '',
     businessName: '',
-    mcc: '',
-    url: '',
     firstName: '',
     lastName: '',
     city: '',
     line1: '',
     postalCode: '',
     state: '',
-    dobDay: '',
-    dobMonth: '',
-    dobYear: '',
+    dob: '',
     phone: '',
     ssnLast4: '',
     statementDescriptor: ''
@@ -57,25 +55,31 @@ function ConnectStripe() {
   };
 
   const handleSubmit = async (e) => {
-    console.log(token)
     e.preventDefault();
-    console.log(formData)
+    // Split the date of birth into day, month, year
+    const dobDay = formData.dob.split('-')[2];
+    const dobMonth = formData.dob.split('-')[1];
+    const dobYear = formData.dob.split('-')[0];
+
+    const formDataToSend = {
+      ...formData,
+      dobDay,
+      dobMonth,
+      dobYear
+    };
+
     try {
       const response = await fetch(`${BASE_URL}/api/stripe/connect-account`, {
         method: "POST",
         headers: {
           Authorization: token,
           "Content-Type": "application/json",
-          
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(formDataToSend),
       });
       const responseData = await response.json();
 
-     
-
-
-      if(responseData.status === 200){
+      if (responseData.status === 200) {
         toast.success(responseData.message, {
           position: "top-right",
           autoClose: 5000,
@@ -86,10 +90,8 @@ function ConnectStripe() {
           progress: undefined,
           theme: "light",
         });
-        setConnected(true)
-        // Handle success
-
-      }else{
+        setConnected(true);
+      } else {
         toast.error(responseData.message, {
           position: "top-right",
           autoClose: 5000,
@@ -100,15 +102,11 @@ function ConnectStripe() {
           progress: undefined,
           theme: "light",
         });
-        return;
       }
-     
+
     } catch (error) {
-
       console.error('Error creating connected account:', error.message);
-      // Handle error
-
-      toast.success(error.message, {
+      toast.error(error.message, {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -123,119 +121,78 @@ function ConnectStripe() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-    <h2 className="text-2xl font-bold mb-4">Connect Stripe Account</h2>
-    {connected ? (
-      <>
-      <p className="mb-4">Your account is connected.</p>
-      <p className="mb-4 ">Check Account Stauts <Link className='text-blue underline' to='/admin/check-account-status'>here..</Link></p>
-      </>
-    ) : (
-      <>
-      <p className="mb-4">Please fill out the below information to connect your account.</p>
-   
-      <form onSubmit={handleSubmit}>
-      {/* Input fields */}
-      <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
-        <div>
-          <label className="block text-sm font-bold ml-3">Country:</label>
-          <input type="text" name="country" value={formData.country} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" name="email" value={formData.email} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-/>
-        </div>
-        <div>
-          <label>Business Type:</label>
-          <input type="text" name="businessType" value={formData.businessType} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Business Name:</label>
-          <input type="text" name="businessName" value={formData.businessName} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-/>
-        </div>
-        <div>
-          <label>MCC:</label>
-          <input type="text" name="mcc" value={formData.mcc} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>URL:</label>
-          <input type="text" name="url" value={formData.url} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>First Name:</label>
-          <input type="text" name="firstName" value={formData.firstName} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Last Name:</label>
-          <input type="text" name="lastName" value={formData.lastName} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-/>
-        </div>
-        <div>
-          <label>City:</label>
-          <input type="text" name="city" value={formData.city} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Address Line 1:</label>
-          <input type="text" name="line1" value={formData.line1} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Postal Code:</label>
-          <input type="text" name="postalCode" value={formData.postalCode} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-/>
-        </div>
-        <div>
-          <label>State:</label>
-          <input type="text" name="state" value={formData.state} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Date of Birth (Day):</label>
-          <input type="text" name="dobDay" value={formData.dobDay} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Date of Birth (Month):</label>
-          <input type="text" name="dobMonth" value={formData.dobMonth} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Date of Birth (Year):</label>
-          <input type="text" name="dobYear" value={formData.dobYear} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Phone:</label>
-          <input type="text" name="phone" value={formData.phone} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>SSN Last 4:</label>
-          <input type="text" name="ssnLast4" value={formData.ssnLast4} onChange={handleChange}   className="input ml-3 p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
-        <div>
-          <label>Statement Descriptor:</label>
-          <input  type="text" name="statementDescriptor" value={formData.statementDescriptor} onChange={handleChange}   className="input p-2 border border-bold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
- />
-        </div>
+      <h2 className="text-2xl font-bold mb-4">Connect Stripe Account</h2>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        {connected ? (
+          <>
+            <p className="mb-4">Your account is connected.</p>
+            <p className="mb-4 ">Check Account Status <Link className='text-blue-500 underline' to='/admin/check-account-status'>here..</Link></p>
+          </>
+        ) : (
+          <>
+            <p className="mb-4">Please fill out the below information to connect your account.</p>
+            <form onSubmit={handleSubmit}>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
+                  <input type="email" name="email" value={formData.email} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Business Type:</label>
+                  <input type="text" name="businessType" value={formData.businessType} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Business Name:</label>
+                  <input type="text" name="businessName" value={formData.businessName} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">First Name:</label>
+                  <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Last Name:</label>
+                  <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">City:</label>
+                  <input type="text" name="city" value={formData.city} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Address Line 1:</label>
+                  <input type="text" name="line1" value={formData.line1} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Postal Code:</label>
+                  <input type="text" name="postalCode" value={formData.postalCode} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">State:</label>
+                  <input type="text" name="state" value={formData.state} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Date of Birth:</label>
+                  <input type="date" name="dob" value={formData.dob} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Phone:</label>
+                  <input type="text" name="phone" value={formData.phone} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">SSN Last 4:</label>
+                  <input type="text" name="ssnLast4" value={formData.ssnLast4} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Statement Descriptor:</label>
+                  <input type="text" name="statementDescriptor" value={formData.statementDescriptor} onChange={handleChange} className="input border border-black rounded-md p-2 mt-2 focus:border-black" />
+                </div>
+              </div>
+              <button type="submit" className="bg-black hover:bg-black text-white font-bold py-2 px-4 mt-3 rounded focus:outline-none focus:shadow-outline">Submit</button>
+            </form>
+          </>
+        )}
       </div>
-      <button type="submit"  className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded d `}>Submit</button>
-    </form>
-
-      </>
-    )}
-   
-
-    <ToastContainer />
-  </div>
+      <ToastContainer />
+    </div>
   );
 }
 
